e6c5df476068b933692dd28b860cb897859c4a60
Enrique Saracho Felix <esarachof97@gmail.com>
2022-04-29 19:59:05 -0600

    Update app.js
    addTask function passes input test to ul

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const task = document.getElementById("task");
   const submit = document.getElementById("submit");
+  const list = document.getElementById("list");
 
   function addTask() {
-      
+    list.innerHTML += '<li>' + task.value + '</li>';
+    
   }
 
   submit.addEventListener("click", addTask);
