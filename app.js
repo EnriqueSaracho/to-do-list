01032dc222dfd6b2f21c2b6a57aa07662e3d6839
Enrique Saracho Felix <esarachof97@gmail.com>
2022-05-06 17:54:55 -0600

    Update app.js
    Cleaned code

--- app.js
+++ app.js
@@ -8,27 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const li = document.createElement("li"); // Creates <li>
     li.innerHTML = input.value;
     ul.appendChild(li);
-    
-    const x = document.createElement("button")
-    x.innerHTML = "x"
+
+    const x = document.createElement("button");
+    x.innerHTML = "x";
     li.appendChild(x);
 
     x.addEventListener("click", () => {
-      x.parentElement.style.display = "none"
-    })
-
-    // const done = document.createElement("input"); // Creates Done button.
-    // done.setAttribute("type", "button");
-    // done.setAttribute("value", "Done");
-    // done.setAttribute("class", "done");
-
-    // list.innerHTML += "<li>" + task.value + " " + done.outerHTML + "</li>"; // Creates task (<li>)
-    // const li = document.createElement("li"); // Makes task an element
-    // task.value = ""; // Clears text input
-
-    // for (i = 0; i < done.length; i++) {
-    //   done[i].addEventListener("click", () => {});
-    // }
+      x.parentElement.style.display = "none";
+    });
   }
 
   // Event listener: clicks submit button when pressing enter on text input.
