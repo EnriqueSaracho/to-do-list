b0727229ce7305d1df84afcd832e1e66f2e25a6b
Enrique Saracho Felix <esarachof97@gmail.com>
2022-05-06 17:01:01 -0600

    Update app.js index.html
    Changed id names in both index.html and app.js

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const task = document.getElementById("task"); // Input: type = text.
+  const input = document.getElementById("input"); // Input: type = text.
   const submit = document.getElementById("submit"); // Input: type = submit.
-  const list = document.getElementById("list"); // Unordered list.
+  const ul = document.getElementById("ul"); // Unordered list.
 
   // Function: adds task from text input to unordered list.
   function addTask() {
-    const done = document.createElement("input"); // Creates Done button.
-    done.setAttribute("type", "button");
-    done.setAttribute("value", "Done");
-    done.setAttribute("class", "done");
+    console.log()
 
-    list.innerHTML += "<li>" + task.value + " " + done.outerHTML + "</li>"; // Creates task (<li>)
-    const li = document.createElement("li"); // Makes task an element
-    console.log(done.parentElement)
-    task.value = ""; // Clears text input
+    // const done = document.createElement("input"); // Creates Done button.
+    // done.setAttribute("type", "button");
+    // done.setAttribute("value", "Done");
+    // done.setAttribute("class", "done");
+
+    // list.innerHTML += "<li>" + task.value + " " + done.outerHTML + "</li>"; // Creates task (<li>)
+    // const li = document.createElement("li"); // Makes task an element
+    // task.value = ""; // Clears text input
 
     // for (i = 0; i < done.length; i++) {
     //   done[i].addEventListener("click", () => {});
@@ -21,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // Event listener: clicks submit button when pressing enter on text input.
-  task.addEventListener("keypress", (event) => {
+  input.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
       submit.click();
     }
